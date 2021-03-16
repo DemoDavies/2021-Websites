71f30c74ade4c4acb24bc3188aeba2d6d1b3b519
DemoDavies <80432393+DemoDavies@users.noreply.github.com>
2021-03-16 14:17:41 +1100

    Delete owen/owen directory

--- owen/owen/scripts/main.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $('#footer').load('/2021-websites/owen/components/footer.html');
-    $('#header').load('/2021-websites/owen/components/header.html', function () {
-        $('#slideoutmenu').load('/2021-websites/owen/components/slideoutmenu.html');
-    });
-})
