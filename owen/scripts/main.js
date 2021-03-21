b32e5332c6117da00f96cea48b3a45da0840efb8
DemoDavies <80432393+DemoDavies@users.noreply.github.com>
2021-03-22 09:20:33 +1100

    Upload owen

---
+++ owen/scripts/main.js
@@ -0,0 +1,36 @@
+var footerXHR= new XMLHttpRequest();
+footerXHR.open('GET', '/2021-websites/owen/components/footer.html', true);
+footerXHR.onreadystatechange= function() {
+    if (this.readyState!==4) return;
+    if (this.status!==200) return;
+    var footerExsists = setInterval(() => {
+        if (document.getElementById('footer')) {
+            clearInterval(footerExsists);
+            document.getElementById('footer').innerHTML= this.responseText;
+        }
+    }, 100);
+};
+footerXHR.send();
+
+var headerXHR= new XMLHttpRequest();
+headerXHR.open('GET', '/2021-websites/owen/components/header.html', true);
+headerXHR.onreadystatechange= function() {
+    if (this.readyState!==4) return;
+    if (this.status!==200) return;
+    document.getElementById('header').innerHTML= this.responseText;
+    var slideoutXHR= new XMLHttpRequest();
+    slideoutXHR.open('GET', '/2021-websites/owen/components/slideOutMenu.html', true);
+    slideoutXHR.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return;
+        document.getElementById('slideoutmenu').innerHTML= this.responseText;
+        console.log('script');
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('src', '/2021-websites/owen/scripts/slideMenu.js');
+        script.setAttribute('defer', '');
+        document.getElementsByTagName('head')[0].appendChild(script);
+    };
+    slideoutXHR.send();
+};
+headerXHR.send();
