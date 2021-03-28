0612c48d727f48844ad6b9740a69f649ca723df5
DemoDavies <80432393+DemoDavies@users.noreply.github.com>
2021-03-29 10:22:16 +1100

    Add files via upload

--- owen/scripts/products.js
+++ owen/scripts/products.js
@@ -7,10 +7,10 @@ function elementTempate (name, description, price, imageSrc) {
     var imageSrcSplit = imageSrc.split('.');
     var src;
     if (imageSrcSplit[1].toLowerCase() != 'webp') src = `src="${imageSrcSplit[0]}_200.${imageSrcSplit[1]}" srcset="${imageSrcSplit[0]}_200.${imageSrcSplit[1]} 1x, ${imageSrcSplit[0]}_300.${imageSrcSplit[1]} 1.5x, ${imageSrcSplit[0]}_400.${imageSrcSplit[1]} 2x,${imageSrcSplit[0]}_500.${imageSrcSplit[1]} 2.5x, ${imageSrcSplit[0]}_600.${imageSrcSplit[1]} 3x"`;
-    else src = `src="${imageSrc}"`;
+    else src = `src="${imageSrc}" height="200"`;
     return `
     <div class="product">
-        <img width="200" ${src} loading="lazy" decoding="async" alt="${name} product image"/>
+        <img width="200" ${src} loading="lazy" decoding="async" alt="${name} product image" width="200"/>
         <div>
             <div>
                 <div class="productName">${name}</div>
